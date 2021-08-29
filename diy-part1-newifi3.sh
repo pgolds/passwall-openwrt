f7185b2692cc6f49d1f0e6dc8ffb6e58f462df87
pgolds <316388173@qq.com>
2021-08-29 17:12:01 +0800

    Update diy-part1-newifi3.sh

--- diy-part1-newifi3.sh
+++ diy-part1-newifi3.sh
@@ -13,6 +13,8 @@ echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600\nauth-ttl=3600" >> packag
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
+git clone --depth=1 https://github.com/Lienol/openwrt-package
+
 git clone https://github.com/sensec/luci-app-udp2raw.git package/luci-app-udp2raw
 git clone https://github.com/pgolds/openwrt-udp2raw.git package/openwrt-udp2raw
 git clone https://github.com/kuoruan/openwrt-kcptun.git package/kcptun
