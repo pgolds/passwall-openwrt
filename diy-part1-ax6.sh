8d5dc64dd6abf45ff343c6f3d12e8ae2b7f6ad5b
pgolds <316388173@qq.com>
2022-03-30 11:49:08 +0800

    Create diy-part1-ax6.sh

---
+++ diy-part1-ax6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
