6efe99147e8eb4488866ba94a71ee06e3fce07e2
pgolds <316388173@qq.com>
2022-04-01 10:31:50 +0800

    Update diy-part2-ax6.sh

--- diy-part2-ax6.sh
+++ diy-part2-ax6.sh
@@ -8,19 +8,21 @@ sed -i 's/192.168.1.1/192.168.111.254/g' package/base-files/files/bin/config_gen
 # 修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
+git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
+
 # themes添加（svn co 命令意思：指定版本如https://github）
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
-git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
-
-
 #添加额外非必须软件包
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/sensec/luci-app-udp2raw.git package/luci-app-udp2raw
 git clone https://github.com/pgolds/openwrt-udp2raw.git package/openwrt-udp2raw
 # git clone https://github.com/kuoruan/openwrt-kcptun.git package/kcptun
 # git clone https://github.com/kuoruan/luci-app-kcptun.git package/luci-app-kcptun
 
 # Add luci-app-onliner
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
+# git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
