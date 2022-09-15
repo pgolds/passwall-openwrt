f1e6662761e70bc460c4a8f0221d557eb45e3a2d
pgolds <316388173@qq.com>
2022-09-15 10:19:18 +0800

    Update diy-part1-ax6.sh

--- diy-part1-ax6.sh
+++ diy-part1-ax6.sh
@@ -7,4 +7,4 @@
 #=================================================
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
